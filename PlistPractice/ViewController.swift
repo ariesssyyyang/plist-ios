742b88bd399fa480eed61714acc8c6f667546332
Aries Yang <ariesssyyyang@gmail.com>
2019-08-05 16:19:04 +0800

    Initial commit

---
+++ PlistPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlistPractice
+//
+//  Created by Aries Yang on 2019/8/5.
+//  Copyright © 2019 Aries Yang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
